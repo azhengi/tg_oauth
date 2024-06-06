5dcfd5b45e8964c1c38c1ac24b2213b0be46c9b1
9opher <9opher@gmail.com>
2024-06-06 16:43:55 +0800

    update: change bot username

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,14 @@ export default function Home() {
         Telegram Login
       </div>
 
-      <Script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="Youm_bot" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></Script>
+      <Script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="hudie_bot" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></Script>
       <Script>
         {
-          `function onTelegramAuth(user) {
-                    alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
-            }`
+          `
+          function onTelegramAuth(user) {
+              alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+          }
+          `
         }
       </Script>
     </main>
