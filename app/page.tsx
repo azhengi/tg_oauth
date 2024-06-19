0fba9a84a2eeb5ca299173df9352cf26b1080f26
9opher <9opher@gmail.com>
2024-06-19 11:09:57 +0800

    update telegram get user

--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,112 @@
 "use client"
-
+import Head from "next/head";
 import Script from "next/script";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+    const [data, setData] = useState("");
+    const [webAppStr, setWebAppStr] = useState("");
 
-  const handleClickJoin = () => {
-    (window as any).Telegram.Login.auth(
-      { bot_id: "5735438655", request_access: true },
-      (data: {
-        auth_date: number;
-        first_name: string;
-        hash: string;
-        id: number;
-        last_name: string;
-        username: string;
-      }) => {
-        if (!data) {
-          // authorization failed
-          // console.log("Auth failed");
-          alert("Auth failed");
-          return;
+    const openAuthTelegram = async () => {
+        const botId = "7274314762";
+        (window as any).Telegram.Login.auth(
+            { bot_id: botId, request_access: true },
+            async (data: {
+                auth_date: number;
+                first_name: string;
+                hash: string;
+                id: number;
+                last_name: string;
+                username: string;
+            }) => {
+                if (!data) {
+                    // authorization failed
+                    console.log("Auth failed");
+                    return;
+                }
+            }
+        );
+    }
+
+    const initTelegramToken = async () => {
+        const tgNodeId = "TG_OAUTH";
+        const telegramWidgetSrc = "https://telegram.org/js/telegram-widget.js?22";
+
+        if (!document.querySelector(tgNodeId)) {
+            const tgScriptNode = document.createElement("script") as HTMLScriptElement;
+            tgScriptNode.id = tgNodeId;
+            tgScriptNode.src = telegramWidgetSrc;
+            tgScriptNode.onload = () => {
+                openAuthTelegram();
+            };
+
+            document.body.appendChild(tgScriptNode);
+        } else {
+            openAuthTelegram();
         }
+    };
 
-        // alert(`Auth success. ${JSON.stringify(data)}`);
+    useEffect(() => {
+        const webAppNodeId = "TG_APP";
+        const telegramWidgetSrc = "https://telegram.org/js/telegram-web-app.js";
 
-        const aLink = document.createElement("a");
-        aLink.setAttribute("href", "https://www.google.com");
-        aLink.setAttribute("target", "_blank");
+        if (!document.querySelector(webAppNodeId)) {
 
+            const webAppNode = document.createElement("script") as HTMLScriptElement;
+            webAppNode.id = webAppNodeId;
+            webAppNode.src = telegramWidgetSrc;
+            webAppNode.onload = () => {
+                if ((window as any)?.Telegram.WebApp) {
+                    const tg = (window as any)?.Telegram.WebApp;
+                    setWebAppStr(() => JSON.stringify(tg));
+                    const initDataUnsafe = tg.initDataUnsafe;
+                    setData(() => JSON.stringify(initDataUnsafe));
+                } else {
+                    console.log('Telegram WebApp is not available.');
+                }
+            };
 
-        setTimeout(() => {
-          aLink.click();
-        }, 500);
+            document.body.appendChild(webAppNode);
+        } else {
+            if ((window as any)?.Telegram.WebApp) {
+                const tg = (window as any)?.Telegram.WebApp;
+                setWebAppStr(() => JSON.stringify(tg));
+                const initDataUnsafe = tg.initDataUnsafe;
+                setData(() => JSON.stringify(initDataUnsafe));
+            } else {
+                console.log('Telegram WebApp is not available.');
+            }
+        }
+        // initTelegramToken();
+    }, [])
 
-        // Here you would want to validate data like described there https://core.telegram.org/widgets/login#checking-authorization
-        // doWhateverYouWantWithData(data);
-      }
+    const handleClickJoin = () => {
+        openAuthTelegram();
+    };
+    return (
+        <>
+            <Head>
+                <Script src="https://telegram.org/js/telegram-web-app.js"></Script>
+            </Head>
+            <main className="flex justify-center mt-[30px] flex-col">
+                <div>
+                    Telegram Login
+                </div>
+
+                <div>
+                    webApp: {webAppStr}
+                </div>
+                <div>
+                    initDataUnsafe: {data}
+                </div>
+
+                <div>
+                    <button onClick={handleClickJoin}>
+                        进入频道
+                    </button>
+                </div>
+            </main>
+        </>
+
     );
-
-  };
-  return (
-    <main className="flex justify-center mt-[30px]">
-      <div>
-        Telegram Login
-      </div>
-
-      <div>
-        <button onClick={handleClickJoin}>
-          进入频道
-        </button>
-      </div>
-
-      <Script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="xiatouman_bot" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></Script>
-      <Script>
-        {
-          `
-          function onTelegramAuth(user) {
-              alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
-          }
-          `
-        }
-      </Script>
-    </main>
-  );
 }
